789547bf604b2556d9338d24d6c57f8b2b5de0ca
Ivaylo Tabakov <ivotabakov1981@gmail.com>
2018-10-05 23:49:00 +0200

    initial commit

---
+++ BicyclesCecoApp/BicyclesCecoApp/Models/CustomerDetails.cs
@@ -0,0 +1,13 @@
+﻿using Realms;
+
+namespace BicyclesCecoApp.Models
+{
+    //  RealmObject means, Implicit reference conversion from model to realmobject
+    public class CustomerDetails : RealmObject
+    {
+        [PrimaryKey]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerAge { get; set; }
+    }
+}
